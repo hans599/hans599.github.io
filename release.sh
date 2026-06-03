800a5651140df6668001a7ed6647ab70338ae6f9
onebear599 <onebear599@qq.com>
2026-06-03 09:57:58 +0800

    更新主题为next，修复数学公式渲染问题

--- release.sh
+++ release.sh
@@ -22,8 +22,10 @@ git commit -m "更新内容"
 git push origin source
 
 # 部署（不用切换分支）
-npx hexo clean && npx hexo deploy
+npx hexo clean && npx hexo generate && npx hexo server 
 
+
+npx hexo deploy
 # 如果报错 cannot find module 'hexo'  
 # 那么先 npm install
 
