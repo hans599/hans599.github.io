af231af595f63f4db01688f48df7481bef69860f
onebear599 <onebear599@qq.com>
2026-06-11 14:32:06 +0800

    更新内容

--- release.sh
+++ release.sh
@@ -1,24 +1,26 @@
-# 更换电脑
-# 克隆source分支到本地
-git clone -b source https://github.com/hans599/hans599.github.io.git 
+# # 更换电脑
+# # 克隆source分支到本地
+# git clone -b source https://github.com/hans599/hans599.github.io.git 
 
-# 进入项目目录
-cd hans599.github.io
+# # 进入项目目录
+# cd hans599.github.io
 
-# 安装依赖
-npm install
+# # 安装依赖
+# npm install
 
-# 安装next主题
-git clone https://github.com/next-theme/hexo-theme-next themes/next
+# # 安装next主题
+# git clone https://github.com/next-theme/hexo-theme-next themes/next
 
-# 将其配置文件拷贝到自己的项目中  如果已经有了 就不需要了
-cp _config.next.yml themes/next/_config.yml
+# # 将其配置文件拷贝到自己的项目中  如果已经有了 就不需要了
+# cp _config.next.yml themes/next/_config.yml
 
+
+# # ----------------------------------------------------------------------------------  #
+
+# # 每次写作前
+# git pull origin source
 
 # ----------------------------------------------------------------------------------  #
-
-# 每次写作前
-git pull origin source
 
 # 写文章...
 
